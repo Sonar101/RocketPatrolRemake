f406f42dc670c1979b708d84f0d24845fa388ea6
Nathann Latimore <nlatimor@ucsc.edu>
2021-04-11 14:40:31 -0700

    Added ships, ship scrolling, and collisions

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,9 +5,9 @@ class Play extends Phaser.Scene {
 
     preload () {
         // load images/tile sprites
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('spaceship', 'assets/spaceship.png');
+        this.load.image('starfield', 'assets/starfield.png');
     }
     
     create() {
@@ -29,11 +29,45 @@ class Play extends Phaser.Scene {
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
         // add rocket (p1)
-        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height/2, 'rocket').setOrigin(0.5, 0); // FIX ME LATER PLZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update()
+        this.p1Rocket.update();         // update player rocket
+        this.ship01.update();           // update spaceships (x3)
+        this.ship02.update();
+        this.ship03.update();
+
+        // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            this.p1Rocket.reset();
+            this.ship03.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            this.p1Rocket.reset();
+            this.ship02.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            this.p1Rocket.reset();
+            this.ship01.reset();
+        }
+    }
+
+    checkCollision(rocket, ship) {
+        // simple AABB checking
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship.y) {
+                return true;
+        } else {
+            return false;
+        }
     }
 }
